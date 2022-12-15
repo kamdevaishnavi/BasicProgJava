a702efba7f4247dd1e42d1974d34410f2ff0e725
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2022-12-15 19:11:07 +0530

    Added Program SquareCube

---
+++ SquareCube.java
@@ -0,0 +1,12 @@
+package com.java.assignmenttask;
+
+public class SquareCube {
+    public static void main(String args[]){
+        int a = 4;
+        int value = a*a*a;
+        int var = a*a;
+        System.out.println("value of the cube=" + value);
+        System.out.println("var of the square=" + var);
+
+    }
+}
