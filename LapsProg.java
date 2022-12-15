6cf17b71a7c1a79e419477e548cbc64f4ce68f83
vaishnavi-kamde <kamdevaishnavi1995@gmail.com>
2022-12-15 19:01:52 +0530

    Added Program For Laps-Program

---
+++ LapsProg.java
@@ -0,0 +1,10 @@
+package com.java.assignmenttask;
+
+public class LapsProg {
+    public static void main(String args[]){
+        int laps = 12;
+        int days = 8;
+        int answer = laps * days;
+        System.out.println(answer);
+    }
+}
